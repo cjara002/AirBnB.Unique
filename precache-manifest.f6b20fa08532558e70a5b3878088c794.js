19f80398569c6523abcad02f448bcb789cbf882c
Carlos Jara <cjjara002@gmail.com>
2020-03-09 11:37:21 -0700

    Updates

---
+++ precache-manifest.f6b20fa08532558e70a5b3878088c794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26435af986cf8b49014f98f70d18bdb",
+    "url": "/AirBnB.Unique/index.html"
+  },
+  {
+    "revision": "624a18fb058518594168",
+    "url": "/AirBnB.Unique/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "8446e381e0e8ea07a8c4",
+    "url": "/AirBnB.Unique/static/css/main.1e296fd9.chunk.css"
+  },
+  {
+    "revision": "624a18fb058518594168",
+    "url": "/AirBnB.Unique/static/js/2.a6c9df40.chunk.js"
+  },
+  {
+    "revision": "8446e381e0e8ea07a8c4",
+    "url": "/AirBnB.Unique/static/js/main.44543df0.chunk.js"
+  },
+  {
+    "revision": "b86f17f775ff47c81efb",
+    "url": "/AirBnB.Unique/static/js/runtime-main.c853eb46.js"
+  }
+]);
